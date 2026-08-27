f2add400d75d69334f76480519be7bcbdbdd7e64
Himanshu Ghuguskar <himanshughuguskar108@gmail.com>
2026-08-28 01:36:18 +0530

    Add Firebase Admin configuration

---
+++ server/config/firebase-admin.js
@@ -0,0 +1,12 @@
+const admin = require("firebase-admin");
+
+if (!admin.apps.length) {
+    admin.initializeApp({
+        credential: admin.credential.applicationDefault()
+    });
+}
+
+const db = admin.firestore();
+const auth = admin.auth();
+
+module.exports = { admin, db, auth };
