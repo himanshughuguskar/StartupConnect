a3c889a692bfc47577407e740279af76bafab159
Himanshu Ghuguskar <himanshughuguskar108@gmail.com>
2026-09-03 01:27:56 +0530

    Fix Firebase Admin private key configuration

--- server/config/firebase-admin.js
+++ server/config/firebase-admin.js
@@ -5,13 +5,18 @@ const dotenv = require("dotenv");
 
 dotenv.config();
 
+const privateKey = Buffer.from(
+    process.env.FIREBASE_PRIVATE_KEY_BASE64,
+    "base64"
+).toString("utf8");
+
 const app = getApps().length
     ? getApps()[0]
     : initializeApp({
         credential: cert({
             projectId: process.env.FIREBASE_PROJECT_ID,
             clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-            privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n")
+            privateKey: privateKey
         })
     });
 
